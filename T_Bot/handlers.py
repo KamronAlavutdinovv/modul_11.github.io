16fe12179476fcc5571d3589f796e402e4e197a0
lpokl <lpokll0@gmil.com>
2024-05-23 15:04:12 +0500

    update

--- T_Bot/handlers.py
+++ T_Bot/handlers.py
@@ -1,8 +1,8 @@
 import os
 from aiogram import Dispatcher, F, Bot
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
-from keyboards import app_kb, buy_ikb
+from keyboards import app_kb
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,44 +19,7 @@ async def start(msg: Message):
     await msg.answer("Salom", reply_markup=app_kb)
 
 
-@dp.message(Command("pay"))
-async def get_btn(msg: Message):
-    text = msg.web_app_data.data
-    product_data = text.split("|")
-    products = {}
-    for i in range(len(product_data)):
-        if len(product_data[i].split("/")) >= 3:
-            title = product_data[i].split("/")[0]
-            price = product_data[i].split("/")[1]
-            quantity = product_data[i].split("/")[2]
-            product = {
-                "title": title,
-                "price": int(price),
-                "quantity": int(quantity)
-            }
-            products[i] = product
-        print(products)
-    await bot.send_invoice(
-        chat_id=msg.chat.id,
-        title="Tolov",
-        description="Tg bot orqali tolov",
-        provider_token=PROVIDER_TOKEN,
-        currency="UZS",
-        payload="Ichki malumot",
-        prices=[
-            LabeledPrice(Lable=f"{product['title']}({product['quantity']})",
-                         amount=(product['price'] * product['quantity']) * 100)
-            for product in products.values()
-        ],
-    )
-
-
-@dp.pre_checkout_query()
-async def pre_checkout_query(checkout_query: PreCheckoutQuery):
-    await bot.answer_pre_checkout_query(checkout_query.id, ok=True)
-
-
-@dp.message(F.func(lambda msg: msg.web_app_data.data))
+@dp.message(F.func(lambda msg: msg.web_app_data.data if msg.web_app_data else None))
 async def get_btn(msg: Message):
     text = msg.web_app_data.data
     product_data = text.split("|")
@@ -65,39 +28,31 @@ async def get_btn(msg: Message):
         if len(product_data[i].split("/")) >= 3:
             title = product_data[i].split('/')[0]
             price = product_data[i].split('/')[1]
-            quantity = int(product_data[i].split('/')[2])
+            quantity = product_data[i].split('/')[2]
             product = {
-                "Nomi": title,
-                "Price": int(price),
-                "Quantity": int(quantity)
+                "title": title,
+                "price": int(price),
+                "quantity": int(quantity)
             }
             products[i] = product
+    print(products)
     await bot.send_invoice(
         chat_id=msg.chat.id,
-        title="To'lov",
-        description="Telegram bot orqali to'lov!",
+        title="Оплата",
+        description="Оплата через Telegram bot",
         provider_token=PROVIDER_TOKEN,
         currency="UZS",
         payload="Ichki malumot",
-        prices=[LabeledPrice(
-            label=f"{product['Nomi']} ({product['Quantity']})",
-            amount=product["Price"] * product["Quantity"] * 100
-        ) for product in products.values()]
-    ),
+        prices=[LabeledPrice(label=f"{product["title"]}({product["quantity"]})",
+                             amount=(product["price"] * product["quantity"]) * 100)
+                for product in products.values()],)
 
-    # @dp.message(F.func(lambda msg: msg.web_app_data.data))
-    # async def get_btn(msg: Message):
-    #     text = msg.web_app_data.data
-    #     products = text.split("|")
-    #     summa = 0
-    #     for i in range(len(products)):
-    #         if len(products[i].split("/")) >= 3:
-    #             title = products[i].split('/')[0]
-    #             price = int(products[i].split('/')[1])
-    #             quantity = int(products[i].split('/')[2])
-    #             await msg.answer(text=f"Nomi: {title}\n"
-    #                                   f"Narxi: {price}\n"
-    #                                   f"Soni: {quantity}\n"
-    #                                   f"Umumiy narxi: {quantity * price}$")
-    #             summa += price * quantity
-    #     await msg.answer(text=f"To'lanishi kerak: {summa}$", reply_markup=buy_ikb)
+
+@dp.pre_checkout_query()
+async def pre_checkout(query: PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(query.id, ok=True)
+
+
+@dp.message(F.func(lambda msg: msg.successful_payment if msg.successful_payment else None))
+async def successful_payment(msg: Message):
+    await msg.answer("To'lov uchun raxmat!")
